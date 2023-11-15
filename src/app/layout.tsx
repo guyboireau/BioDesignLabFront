75cb04fed7cc138a9b8bf9e7d6eeec6ce02d3740
Quidi boz <boireauguy@gmail.com>
2023-11-15 14:07:35 +0100

    init navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navigation from "./components/navigation/index.client";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
+        <Navigation />
         <body className={inter.className}>{children}</body>
       </Providers>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import TagList from "@/components/TagList";
 
-export default function BlogArticle() {
+export default function Home() {
   return (
-    <div className="container">
+    <div className="container mx-auto px-4">
       <h1>Blog</h1>
       <TagList />
+      <p>This is a content to make our page longer</p>
+      <div className="w-full h-screen bg-green-300"></div>
+      <p>Lorem Ipsum is simply dummy text ...</p>
+      <div className="w-full h-screen bg-green-300"></div>
     </div>
   );
 }
