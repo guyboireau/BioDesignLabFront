7e15e39798e1e026af587e8fe9972e70c2ed6926
Sophie <sophie@nalyze.team>
2023-12-13 11:07:36 +0100

    feat: font size + color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,9 @@ import TagList from "@/components/TagList";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4 flex h-full">
-      <div className="w-full flex-1 bg-green-300">
-        <h1>Blog</h1>
-        <TagList />
-      </div>
+    <div className="container mx-auto p-24 flex-1 bg-green-300">
+      <h1>Blog</h1>
+      <TagList />
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ const config: Config = {
       fontFamily: {
         "noto-sans": ["var(--font-noto-sans)"],
       },
+      colors: {
+        "dark-purple": { DEFAULT: "#140821", dark: "#140821" },
+        "light-green": { DEFAULT: "#76E581", dark: "#76E581" },
+        "royal-blue": { DEFAULT: "#002266", dark: "#002266" },
+        "argentinian-blue": { DEFAULT: "#65AFFF", dark: "#65AFFF" },
+        "alice-gray": { DEFAULT: "#E5EBF0", dark: "#E5EBF0" },
+      },
     },
   },
   plugins: [],
