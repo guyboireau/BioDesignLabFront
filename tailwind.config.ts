52b79d1f527b45377d4a4a6abfe70bf1ce8b42a7
Pan Sophie <75846673+sophie-pan@users.noreply.github.com>
2023-12-13 12:12:03 +0100

    feat: typography + color (#6)
    * feat: font family
    
    * feat: font size + color
    
    ---------
    
    Co-authored-by: Sophie <sophie@nalyze.team>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "noto-sans": ["var(--font-noto-sans)"],
+      },
+      colors: {
+        "dark-purple": { DEFAULT: "#140821", dark: "#140821" },
+        "light-green": { DEFAULT: "#76E581", dark: "#76E581" },
+        "royal-blue": { DEFAULT: "#002266", dark: "#002266" },
+        "argentinian-blue": { DEFAULT: "#65AFFF", dark: "#65AFFF" },
+        "alice-gray": { DEFAULT: "#E5EBF0", dark: "#E5EBF0" },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
