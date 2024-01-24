30b9b3e0d78c7a3222e583f27336b782ef5aad48
Pan Sophie <75846673+sophie-pan@users.noreply.github.com>
2024-01-24 16:58:43 +0100

    feat: responsive font size + main layout (#13)
    * feat: responsive font size
    
    * feat: main layout
    
    ---------
    
    Co-authored-by: Sophie <sophie@nalyze.team>

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import Button from "./Button";
-import exp from "constants";
 
 const Navbar = () => {
   return (
     <>
       <div className="w-full h-20 sticky top-0 header">
-        <div className="container mx-auto px-4 h-full">
+        <div className="container h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
             <ul className="hidden md:flex gap-x-6 text-white">
