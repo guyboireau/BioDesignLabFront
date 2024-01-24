b41b045fd094be3f72f534a97516cb37eac72b96
BastienBenech <bastbenech@gmail.com>
2024-01-24 17:26:38 +0100

    fix navbar with menu anchor and refactor hover

--- src/app/components/navigation/navbar/Menu.tsx
+++ src/app/components/navigation/navbar/Menu.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Menu = ({title, link}: {title: string, link: string}) => {
+    const pathname = usePathname();
 
     return (
         <Link href={link} className="group">
         <svg width="94" height="58" viewBox="0 0 94 58" fill="none">
-          <g clipPath="url(#clip0_249_365)">
-            <path className="invisible group-hover:visible group-active:visible" d="M81.2365 47.4795L50.9402 56.3696C50.9402 56.3696 41.0612 60.27 28.1489 54.465C21.4589 51.4572 12.8484 47.6481 12.8484 47.6481C12.8484 47.6481 4.25915 44.6894 1.76458 34.0916C-0.729995 23.5008 -0.722908 18.8273 8.61756 13.7182C17.9651 8.61601 29.3608 2.2067 29.3608 2.2067C29.3608 2.2067 35.6893 -2.24889 47.9921 2.65648C60.2949 7.56185 80.2869 16.2833 80.2869 16.2833C80.2869 16.2833 86.0769 18.4759 91.392 29.5306C96.5158 40.1917 92.8164 44.2537 81.2365 47.4724V47.4795Z" stroke="#76E581" strokeMiterlimit="10"/>
-              <text className="group-hover:fill-light-green" x="12" y="35" fill="white">{title}</text>
-          </g>
+            <path className={pathname == link ? "visible" : "invisible"} d="M81.2365 47.4795L50.9402 56.3696C50.9402 56.3696 41.0612 60.27 28.1489 54.465C21.4589 51.4572 12.8484 47.6481 12.8484 47.6481C12.8484 47.6481 4.25915 44.6894 1.76458 34.0916C-0.729995 23.5008 -0.722908 18.8273 8.61756 13.7182C17.9651 8.61601 29.3608 2.2067 29.3608 2.2067C29.3608 2.2067 35.6893 -2.24889 47.9921 2.65648C60.2949 7.56185 80.2869 16.2833 80.2869 16.2833C80.2869 16.2833 86.0769 18.4759 91.392 29.5306C96.5158 40.1917 92.8164 44.2537 81.2365 47.4724V47.4795Z" stroke="#76E581" strokeMiterlimit="10"/>
+              <text className={pathname == link ? "fill-light-green" : "group-hover:fill-light-green"} x="12" y="35" fill="white">{title}</text>
           <defs>
-            <clipPath id="clip0_249_365">
               <rect width="94" height="58" fill="white"/>
-            </clipPath>
           </defs>
         </svg>
       </Link>
--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import Logo from "./Logo";
 import Button from "./Button";
 import Menu from "./Menu";
@@ -7,21 +6,21 @@ import Menu from "./Menu";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-dark-purple sticky top-0">
+      <div className="w-full h-20 bg-dark-purple sticky top-0 header">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
-            <ul className="flex md:flex gap-x-6 text-white">
-              <li>
-                <Menu title="About us" link="/about"/>
-              </li>
-              <li> 
-                <Menu title="Library" link="/library"/>
-              </li>
-              <li>
-                <Menu title="Generator" link="/generator"/>
-              </li>
-            </ul>
+              <ul className="hidden md:flex gap-x-6 text-white">
+                <li>
+                  <Menu title="About us" link="/about"/>
+                </li>
+                <li>
+                  <Menu title="Library" link="/library"/>
+                </li>
+                <li>
+                  <Menu title="Generator" link="/generator"/>
+                </li>
+              </ul>
             <Button />
           </div>
         </div>
