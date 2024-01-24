c2d1dc5c83f1be36a24ada6b11f31480e14d4797
Sophie <sophie@nalyze.team>
2024-01-24 16:54:15 +0100

    feat: main layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,8 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
-        'darkblue': '#140821',
+        'dark-blue': '#140821',
       }
     },
   },
