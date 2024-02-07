c397258fd673188f753ea3e307226af5458d6389
BastienBenech <bastbenech@gmail.com>
2024-02-07 10:30:40 +0100

    refacto: replace Dark purple by Dark blue because it was the same color

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -7,7 +7,7 @@ import Menu from "./Menu";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-dark-purple sticky top-0 header">
+      <div className="w-full h-20 bg-dark-blue sticky top-0 header">
         <div className="container mx-auto px-4 h-full">
 
           <div className="flex justify-between items-center h-full">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,6 @@ const config: Config = {
         'noto-sans': ['var(--font-noto-sans)'],
       },
       colors: {
-        'dark-purple': { DEFAULT: '#140821', dark: '#140821' },
         'light-green': { DEFAULT: '#76E581', dark: '#76E581' },
         'royal-blue': { DEFAULT: '#002266', dark: '#002266' },
         'argentinian-blue': { DEFAULT: '#65AFFF', dark: '#65AFFF' },
