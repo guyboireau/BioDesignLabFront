32541ef202b34313d5089bb5153f00296764cb46
guyboireau <boireauguy@gmail.com>
2024-03-14 15:15:21 +0100

    fix warning

--- src/app/components/navigation/navbar/Logo.tsx
+++ src/app/components/navigation/navbar/Logo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import headerLogo from '../../../../images/logo.svg';
@@ -11,13 +11,13 @@ const Logo = () => {
     setWidth(window.innerWidth);
   };  
 
-  const changeNavButton = () => {
+  const changeNavButton = useCallback(() => {
     if (window.scrollY >= 400 && width < 768) {
       setShowButton(true);
     } else {
       setShowButton(false);
     }
-  };
+  }, [width]);
 
   useEffect(() => {
     window.addEventListener("resize", updateWidth);
@@ -29,13 +29,11 @@ const Logo = () => {
       window.removeEventListener("resize", updateWidth);
       window.removeEventListener("scroll", changeNavButton);
     };
-  }, [width]);
+  }, [width, changeNavButton]);
 
   return (
-    
-    <Link href="/"
-      style={{ display: showButton ? "none" : "block" }}>
-        <Image src={headerLogo} alt="BioDesign Lab Logo"/>
+    <Link href="/" style={{ display: showButton ? "none" : "block" }}>
+      <Image src={headerLogo} alt="BioDesign Lab Logo"/>
     </Link>
   );  
 };
